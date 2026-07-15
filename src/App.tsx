29a603bf30d889c36efdb853d585c52522088733
[dyad] <hirakroya50@gmail.com>
2026-07-15 15:42:46 +0530

    [dyad] Standardizing React versions and imports to fix hook errors - wrote 3 file(s)

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react'
+import React, { useState, useEffect } from 'react'
 import { BrowserRouter as Router, Routes, Route, Link, Navigate } from 'react-router-dom'
 import { Toaster } from 'react-hot-toast'
 import { AuthProvider, useAuth } from './components/AuthProvider'
